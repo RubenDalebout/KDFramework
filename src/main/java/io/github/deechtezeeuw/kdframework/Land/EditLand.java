3acb04e121fcab98102ce34ac379446442d8e8d9
DeEchteZeeuw <rubendalebout@gmail.com>
2021-11-04 15:12:23 +0100

    Additional updates to /k land edit

--- src/main/java/io/github/deechtezeeuw/kdframework/Land/EditLand.java
+++ src/main/java/io/github/deechtezeeuw/kdframework/Land/EditLand.java
@@ -36,6 +36,9 @@ public class EditLand {
             return;
         }
 
+        // current land values
+        Land land = plugin.SQLSelect.land_get_by_name(args[2]);
+
         // Check what he wants to edit
         if (args.length >= 4) {
             // Check if its not one of those
@@ -55,27 +58,52 @@ public class EditLand {
         if (args.length == 5) {
             // Prefix check
             if (args[3].equalsIgnoreCase("prefix")) {
+                // Check if it has more then 36 characters
                 if (args[4].length() > 36) {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             plugin.Config.getGeneralPrefix() + "&cJe prefix bevat meer dan het maximum van &4&l36 &ckarakters!"));
+                    return;
+                }
+
+                // Check if its not the same as the current value
+                if (land.getPrefix().equalsIgnoreCase(args[4])) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&cJe prefix is al &4&l"+args[4]+"&c!"));
                     return;
                 }
             }
 
             // Invite check
             if (args[3].equalsIgnoreCase("invite")) {
+                // Check if new value is not true or false
                 if (!args[4].equalsIgnoreCase("true") && !args[4].equalsIgnoreCase("false")) {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             plugin.Config.getGeneralPrefix() + "&cJe invite waarde kan alleen &4&ltrue&c/&4&lfalse &czijn!"));
+                    return;
+                }
+
+                // Check if its the same as the current value
+                Boolean newValue = args[4].equalsIgnoreCase("true") ? true : false;
+                if (land.getInvite() == newValue) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&cJe invite staat al op &4&l"+land.getInvite()+"&c!"));
                     return;
                 }
             }
 
             // Maximum check
             if (args[3].equalsIgnoreCase("maximum")) {
+                // Check if its number is higher then 0
                 if (!(Integer.parseInt(args[4]) > 0)) {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             plugin.Config.getGeneralPrefix() + "&cJe maximum waarde kan alleen &4&l1 of hoger &czijn!"));
+                    return;
+                }
+
+                // Check if its new number is the same as the current
+                if (land.getMaximum() == Integer.parseInt(args[4])) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&cJe maximum is al &4&l"+args[4]+"&c!"));
                     return;
                 }
             }
@@ -87,5 +115,7 @@ public class EditLand {
         }
 
         plugin.SQLUpdate.update_land(args[2], args[3], args[4]);
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                plugin.Config.getGeneralPrefix() + "&aSuccesvol de &2&l"+args[3]+" &aaangepast naar &2&l"+args[4]+" &avan het land &2&l"+args[2]+"&a!"));
     }
 }
