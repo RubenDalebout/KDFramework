2e01046c804c27d13e77132e9ae8de3f1b0af2c3
DeEchteZeeuw <rubendalebout@gmail.com>
2021-11-10 13:09:11 +0100

    /k rank edit <kingdom> <rank> <column> <value> Fixed

--- src/main/java/io/github/deechtezeeuw/kdframework/Rank/EditRank.java
+++ src/main/java/io/github/deechtezeeuw/kdframework/Rank/EditRank.java
@@ -33,6 +33,130 @@ public class EditRank {
             sender.sendMessage("Rank: "+ArgRank);
             sender.sendMessage("Column: "+ArgWhat);
             sender.sendMessage("Value: "+ArgValue);
+
+            // Check if Kingdom exists
+            if (!plugin.SQLSelect.land_exists(ArgKingdom)) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&cHet land &4&l"+ArgKingdom+" &cbestaat niet!"));
+                return;
+            }
+            Land land = plugin.SQLSelect.land_get_by_name(ArgKingdom);
+            // Check if rank exists in kingdom
+            boolean found = false;
+            Rank editRank = null;
+            for (Rank rank : land.getRanks()) {
+                if (rank.getName().equalsIgnoreCase(ArgRank)) {
+                    found = true;
+                    editRank = rank;
+                }
+            }
+            if (!found) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&cDe rank &4&l"+ArgRank+" &cbestaat niet in het land &4&l"+land.getName()+"&c!"));
+                return;
+            }
+
+            // Check if ArgWhat is prefix/level/maximum/default
+            if (!ArgWhat.equalsIgnoreCase("prefix") && !ArgWhat.equalsIgnoreCase("level") && !ArgWhat.equalsIgnoreCase("maximum") && !ArgWhat.equalsIgnoreCase("default")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&4&l"+ArgWhat+" &cis niet toegestaan, mag alleen: &4&lprefix&c, &4&llevel&c, &4&lmaximum &cof &4&ldefault &czijn!"));
+                return;
+            }
+
+            // Check if its prefix the length is not longer then 36
+            if (ArgWhat.equalsIgnoreCase("prefix")) {
+                if (ArgValue.length() > 36) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&4&l"+ArgValue+" &cis niet toegestaan, je totale karakters mag niet langer zijn dan &4&l36 &ctekens!"));
+                    return;
+                }
+
+                // Update prefix
+                plugin.SQLUpdate.update_rank(editRank, ArgWhat, ArgValue);
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&aDe rank &2&l"+editRank.getName()+" &aheeft nu de prefix &2&l"+ArgValue+" &ain het land &2&l"+land.getName()+"&a!"));
+                return;
+            }
+
+            // Check if its level is an number
+            if (ArgWhat.equalsIgnoreCase("level")) {
+                // Check if argnew is number
+                if (!(StringUtils.isNumeric(ArgValue))) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&4&l"+ArgValue+" &cis niet toegestaan, je nieuwe &4&llevel &cmoet een &4&lgetal &czijn!"));
+                    return;
+                }
+                Integer newValue = Integer.parseInt(ArgValue);
+
+                // Update level
+                plugin.SQLUpdate.update_rank(editRank, ArgWhat, newValue.toString());
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&aDe rank &2&l"+editRank.getName()+" &aheeft nu een level van &2&l"+newValue+" &ain het land &2&l"+land.getName()+"&a!"));
+                return;
+            }
+
+            // Check if its maximum is an number
+            if (ArgWhat.equalsIgnoreCase("maximum")) {
+                // Check if argnew is number
+                if (!(StringUtils.isNumeric(ArgValue))) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&4&l"+ArgValue+" &cis niet toegestaan, je nieuwe &4&lmaximum &cmoet een &4&lgetal &czijn!"));
+                    return;
+                }
+                Integer newValue = Integer.parseInt(ArgValue);
+                // Check if this rank is the default rank
+                if (editRank.getKdDefault()) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&cDeze rank mag geen maximum hebben, omdat het de default rank is!"));
+                    return;
+                }
+
+                // check if Argnew is lower then rank members
+                Integer members = 0;
+                for (Speler spieler : land.getLeden()) {
+                    if (spieler.getRank().equals(editRank.getUuid())) {
+                        members = members+1;
+                    }
+                }
+
+                if (newValue < members) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&4&l"+ArgValue+" &cis niet toegestaan, je nieuwe maximum moet hoger zijn of gelijk zijn aan &4&l"+members+" &c!"));
+                    return;
+                }
+
+                // Update maximum
+                plugin.SQLUpdate.update_rank(editRank, ArgWhat, newValue.toString());
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&aDe rank &2&l"+editRank.getName()+" &aheeft nu het maximum van &2&l"+newValue+" &ain het land &2&l"+land.getName()+"&a!"));
+                return;
+            }
+
+            // Check if its default and its true or false
+            if (ArgWhat.equalsIgnoreCase("default")) {
+                if (!ArgValue.equalsIgnoreCase("true")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&4&l"+ArgValue+" &cis niet toegestaan, je nieuwe &4&ldefault &cmoet &4&ltrue &czijn!"));
+                    return;
+                }
+
+                Rank oldDefault = land.get_defaultRank();
+                // Check if its the same old rank
+                if (ArgWhat.equalsIgnoreCase("true") && editRank.equals(oldDefault)) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            plugin.Config.getGeneralPrefix() + "&cDe rank &4&l"+editRank.getName()+" &cis al de default rank!"));
+                    return;
+                }
+
+                // New defaultrank
+                plugin.SQLUpdate.update_rank(editRank, ArgWhat, ArgValue);
+                // Old defaultrank
+                plugin.SQLUpdate.update_rank(oldDefault, ArgWhat, "false");
+
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        plugin.Config.getGeneralPrefix() + "&aDe rank &2&l"+editRank.getName()+" &ais nu de default rank in het land &2&l"+land.getName()+"&a!"));
+                return;
+            }
             return;
         }
 
